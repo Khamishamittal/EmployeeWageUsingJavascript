266e11786b11e8f02dc63d219cb606cead84df5e
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-03-29 15:57:56 +0530

    Check weather Employee is present or absent

--- Employee.js
+++ Employee.js
@@ -1,1 +1,11 @@
 console.log("Welcome to Employee Wage Problem Using JavaScript")
+
+//UC-01----->Ability to Check Employee is present or Absent
+const IS_Absent = 0
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_Absent) {
+    console.log("Employee is Absent");
+}
+else {
+    console.log("Employee is Present");
+}
