d6d6bbd616fc694a2a1205571b2242f0f346ad84
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-03-29 19:49:45 +0530

    Write a Program to Calculate Monthly Wage

--- Employee.js
+++ Employee.js
@@ -1,1 +1,12 @@
 console.log("Welcome to Employee Wage Problem Using JavaScript")
+
+//UC-04------->Calculating Wages for a Month assuming 20 Working Days in a Month
+let totalEmpHrs=0;
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
+{
+  let empCheck=Math.floor(Math.random()*10)%3;
+  totalEmpHrs+=getWorkingHours(empCheck);
+}
+
+let empWage=totalEmpHrs*WAGE_PER_HOUR;
+console.log("Hour: "+totalEmpHrs+" Emp Wage: "+empWage);
